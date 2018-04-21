942b7e85a9d8f1b5a6811e2c9dd366acc7362215
Ítalo Dell Areti <italo@Caxu.local>
2018-04-21 13:02:10 -0300

    Modificado

--- app/src/main/java/com/mtgo007/movies/MainActivity.java
+++ app/src/main/java/com/mtgo007/movies/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
